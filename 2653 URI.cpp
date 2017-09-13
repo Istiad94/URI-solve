a77c09f589ad8c50165541c58feb5cb6efcff7f2
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-09-14 03:42:09 +0600

    Create 2653 URI.cpp

---
+++ 2653 URI.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define INF 99999
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+
+    string a;
+    set<string>ans;
+
+    while(cin>>a)
+        ans.insert(a);
+
+    cout<<ans.size()<<endl;
+
+    return 0;
+}
